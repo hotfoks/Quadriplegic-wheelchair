b17b0ebaa0335a9aa01cd6447afd0be019c1cb9c
Benjamin Amendolagine <benjamin@raspberrypi>
2024-02-21 09:34:34 -0800

    my chagis are that i clend it up and made it mor cleer

--- Sense_HAT_C_Pi/RaspberryPi/IMU/bcm2835/examples/main.c
+++ Sense_HAT_C_Pi/RaspberryPi/IMU/bcm2835/examples/main.c
@@ -1,54 +1,22 @@
 #include "IMU.h"
-
-void avrig (int x, int y, int z){
-        static float tolal5x = 0.0;
-        static float tolal5y = 0.0;
+    void absloot (double pitch, double Roll, double z){
+        static float totalPitch = 0.0;
+        static float totalRoll = 0.0;
         static float tolal5z = 0.0;
         static int BestCounter =0;
         if(BestCounter==4){           
             
             
-            tolal5x = tolal5x + x;
-             tolal5x = tolal5x / 5.0;
-            printf("avrig=%f\n", tolal5x);
+            totalPitch = totalPitch + pitch;
+             totalPitch = totalPitch / 5.0;
+            printf("absloot=%f\n", totalPitch);
             BestCounter =0;
-            tolal5x == 0.0;
+            totalPitch == 0.0;
             
-             tolal5y = tolal5y + y;
-             tolal5y = tolal5y / 5.0;
-            printf("avrig=%f\n", tolal5y);
-            tolal5y == 0.0;
-            
-              tolal5z  = tolal5z + z;
-             tolal5z = tolal5z / 5.0;
-            printf("avrig=%f\n", tolal5z);
-            tolal5z == 0.0;
-            
-        } else {
-            tolal5z = tolal5z + z;
-            tolal5y = tolal5y + y;
-            tolal5x = tolal5x + x;
-            BestCounter +=1;
-        }
-}
-    void absloot (double x, double y, double z){
-        static float tolal5x = 0.0;
-        static float tolal5y = 0.0;
-        static float tolal5z = 0.0;
-        static int BestCounter =0;
-        if(BestCounter==4){           
-            
-            
-            tolal5x = tolal5x + x;
-             tolal5x = tolal5x / 5.0;
-            printf("absloot=%f\n", tolal5x);
-            BestCounter =0;
-            tolal5x == 0.0;
-            
-             tolal5y = tolal5y + y;
-             tolal5y = tolal5y / 5.0;
-            printf("absloot=%f\n", tolal5y);
-            tolal5y == 0.0;
+             totalRoll = totalRoll + Roll;
+             totalRoll = totalRoll / 5.0;
+            printf("absloot=%f\n", totalRoll);
+            totalRoll == 0.0;
             
               tolal5z  = tolal5z + z;
              tolal5z = tolal5z / 5.0;
@@ -57,12 +25,12 @@ void avrig (int x, int y, int z){
             
         } else {
             tolal5z = tolal5z + z;
-            tolal5y = tolal5y + y;
-            tolal5x = tolal5x + x;
+            totalRoll = totalRoll + Roll;
+            totalPitch = totalPitch + pitch;
             BestCounter +=1;
         }
 }
-
+// M7;7#
     
 int main()
 {
@@ -84,7 +52,7 @@ int main()
         
 		temp = QMI8658_readTemp();
         
-        avrig(stGyroRawData.s16X, stGyroRawData.s16Y, stGyroRawData.s16Z);
+        
         absloot(stAngles.fPitch, stAngles.fRoll, stAngles.fYaw);
         
         bcm2835_delay(100);
@@ -92,3 +60,104 @@ int main()
   
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
