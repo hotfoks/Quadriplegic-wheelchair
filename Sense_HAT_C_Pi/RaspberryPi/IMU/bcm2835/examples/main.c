33f55d6188859db8cb98974ea417a2b0a8f4a60e
Benjamin Amendolagine <benjamin@raspberrypi>
2024-02-23 09:41:09 -0800

    we conektid moshin comand to forwerds and bakwerds moters using pitch

--- Sense_HAT_C_Pi/RaspberryPi/IMU/bcm2835/examples/main.c
+++ Sense_HAT_C_Pi/RaspberryPi/IMU/bcm2835/examples/main.c
@@ -1,4 +1,4 @@
-#include "IMU.h"
+# include "IMU.h"
     void absloot (double pitch, double Roll, double z){
         static float totalPitch = 0.0;
         static float totalRoll = 0.0;
@@ -7,22 +7,24 @@
         if(BestCounter==4){           
             
             
-            totalPitch = totalPitch + pitch;
+             totalPitch = totalPitch + pitch;
              totalPitch = totalPitch / 5.0;
-            printf("absloot=%f\n", totalPitch);
-            BestCounter =0;
-            totalPitch == 0.0;
+               //totalPitch = totalPitch + 38.0; 
+             printf("M %f;%f#\n", totalPitch,totalPitch );
+            // printf("absloot=%f\n", totalPitch);
+             BestCounter =0;
+             totalPitch = 0.0;
             
              totalRoll = totalRoll + Roll;
              totalRoll = totalRoll / 5.0;
-            printf("absloot=%f\n", totalRoll);
-            totalRoll == 0.0;
+             //printf("absloot=%f\n", totalRoll);
+             totalRoll = 0.0;
             
               tolal5z  = tolal5z + z;
              tolal5z = tolal5z / 5.0;
-            printf("absloot=%f\n", tolal5z);
-            tolal5z == 0.0;
-            
+          //   printf("absloot=%f\n", tolal5z);
+             tolal5z = 0.0;
+       
         } else {
             tolal5z = tolal5z + z;
             totalRoll = totalRoll + Roll;
@@ -60,59 +62,6 @@ int main()
   
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
